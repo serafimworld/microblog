a96966710081da7181a6f1665c543806c39c0202
Artem Karpov <tofractal@gmail.com>
2022-01-15 16:32:58 +0300

    fix bag

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQlAlchemy(app)
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from app import routes, models
