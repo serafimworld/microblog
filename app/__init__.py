676a1ee72fcad9a34d99b5da16e72d15f983ea38
Artem Karpov <tofractal@gmail.com>
2022-01-15 14:34:44 +0300

    added db, migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQlAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQlAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes
+from app import routes, models
