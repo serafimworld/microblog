8ddbf5526bf21a6af87105b6accd2fd2c6bb1534
Artem Karpov <tofractal@gmail.com>
2022-01-16 17:41:24 +0300

    Logout view function

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import render_template, flash, redirect, url_for
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user
 from app.models import User
 from app import app
 from app.forms import LoginForm
@@ -38,3 +38,8 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
