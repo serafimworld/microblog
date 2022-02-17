08d427845eafdba06ef70ae101cc14e53567d982
Artem Karpov <tofractal@gmail.com>
2022-02-17 19:14:36 +0300

    fix bug

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 from config import Config
 
 
